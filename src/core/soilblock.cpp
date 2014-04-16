cbebe8d8908a0bdf3b46aee12137eafc2f6903e3
Denis Pesotsky <denis@kde.ru>
2014-04-16 23:23:07 +0400

    SoilBlock: lil' cleanup

--- src/core/soilblock.cpp
+++ src/core/soilblock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2013  Denis Pesotsky, Maxim Torgonsky
+ * Copyright (C) 2010-2014  Denis Pesotsky, Maxim Torgonsky
  *
  * This file is part of QFrost.
  *
@@ -72,181 +72,14 @@ SoilBlock::SoilBlock(const double &width, const double &height):
     mDimensions.push_back(width);
     mDimensions.push_back(height);
 
-    // грунт А (незасол каол)
-    /*mCapacity = Phased(2800000, 2150000);
-    mConductivity = Phased(1.5, 1.59);
-    mMoistureTotal = 0.3;
-    mDryDensity = 1400;
-    mUnfrozenWaterCurve[-0.62] = 59.6 / 100.0;
-    mUnfrozenWaterCurve[-0.80] = 50.0 / 100.0;
-    mUnfrozenWaterCurve[-0.90] = 18.0 / 100.0;
-    mUnfrozenWaterCurve[-1.12] = 14.0 / 100.0;
-    mUnfrozenWaterCurve[-1.90] = 10.1 / 100.0;
-    mUnfrozenWaterCurve[-3.00] = 7.50 / 100.0;
-    mUnfrozenWaterCurve[-6.00] = 4.70 / 100.0;
-    mUnfrozenWaterCurve[-10.0] = 3.05 / 100.0;
-    mUnfrozenWaterCurve[-13.5] = 2.25 / 100.0;
-    mUnfrozenWaterCurve[-15.9] = 1.95 / 100.0;
-    mUnfrozenWaterCurve[-20.0] = 1.65 / 100.0;
-    mUnfrozenWaterCurve[-29.8] = 1.60 / 100.0;/
-
-    // Плохая кривая для грунта А
-    /*
-    mUnfrozenWaterCurve[-0.13] = 60.0 / 100.0;
-    mUnfrozenWaterCurve[-0.15] = 45.0 / 100.0;
-    mUnfrozenWaterCurve[-0.16] = 25.0 / 100.0;
-    mUnfrozenWaterCurve[-0.17] = 20.0 / 100.0;
-    mUnfrozenWaterCurve[-0.30] = 11.5 / 100.0;
-    mUnfrozenWaterCurve[-0.50] = 8.0 / 100.0;
-    mUnfrozenWaterCurve[-1.00] = 5.2 / 100.0;
-    mUnfrozenWaterCurve[-2.00] = 3.0 / 100.0;
-    mUnfrozenWaterCurve[-3.00] = 2.0 / 100.0;
-    mUnfrozenWaterCurve[-4.00] = 1.5 / 100.0;
-    mUnfrozenWaterCurve[-7.00] = 1.4 / 100.0;*/
-
-    // грунт Б (засол каол)
-
-    /*mCapacity = Phased(2800000, 2150000);
-    mConductivity = Phased(1.5, 1.59);
-    mMoistureTotal = 0.3;
-    mDryDensity = 1400;
-
-    mUnfrozenWaterCurve[-2.05] =    60.5 / 100.0;
-    mUnfrozenWaterCurve[-2.25] =    50.5 / 100.0;
-    mUnfrozenWaterCurve[-2.35] =    44.0 / 100.0;
-    mUnfrozenWaterCurve[-2.60] =    34.0 / 100.0;
-    mUnfrozenWaterCurve[-3.15] =    25.0 / 100.0;
-    mUnfrozenWaterCurve[-3.75] =    21.0 / 100.0;
-    mUnfrozenWaterCurve[-5.25] =    16.0 / 100.0;
-    mUnfrozenWaterCurve[-9.00] =    10.0 / 100.0;
-    mUnfrozenWaterCurve[-12.5] =    7.0 / 100.0;
-    mUnfrozenWaterCurve[-17.0] =    5.2 / 100.0;
-    mUnfrozenWaterCurve[-21.5] =    4.1 / 100.0;
-    mUnfrozenWaterCurve[-30.0] =    3.1 / 100.0;*/
-
-    // Плохая кривая для грунта Б
-    /*
-    mUnfrozenWaterCurve[-0.40] = 61.2 / 100.0;
-    mUnfrozenWaterCurve[-0.50] = 34.1 / 100.0;
-    mUnfrozenWaterCurve[-1.00] = 16.5 / 100.0;
-    mUnfrozenWaterCurve[-2.00] = 9.0 / 100.0;
-    mUnfrozenWaterCurve[-3.00] = 5.8 / 100.0;
-    mUnfrozenWaterCurve[-4.00] = 4.3 / 100.0;
-    mUnfrozenWaterCurve[-5.00] = 3.4 / 100.0;
-    mUnfrozenWaterCurve[-6.00] = 3.0 / 100.0;*/
-
-    /*
-    // грунт В (засол монт)
-    mCapacity = Phased(3370000, 2210000);
-    mConductivity = Phased(1.55, 1.70);
-    mMoistureTotal = 0.6;
-    mDryDensity = 1600;
-    mUnfrozenWaterCurve[-1.39] = 135.0 / 100.0;
-    mUnfrozenWaterCurve[-1.45] = 105.0 / 100.0;
-    mUnfrozenWaterCurve[-1.75] = 77.0 / 100.0;
-    mUnfrozenWaterCurve[-2.30] = 60.0 / 100.0;
-    mUnfrozenWaterCurve[-3.00] = 50.0 / 100.0;
-    mUnfrozenWaterCurve[-5.00] = 38.5 / 100.0;
-    mUnfrozenWaterCurve[-6.50] = 35.0 / 100.0;
-    mUnfrozenWaterCurve[-8.00] = 33.0 / 100.0;
-    mUnfrozenWaterCurve[-9.60] = 31.7 / 100.0;
-    mUnfrozenWaterCurve[-14.5] = 29.9 / 100.0;
-    mUnfrozenWaterCurve[-19.45] = 28.1 / 100.0;
-    mUnfrozenWaterCurve[-20.44] = 27.9 / 100.0;
-    */
-
-    //19
-    /*
-    mCapacity = Phased(2172560, 1542860);
-    mConductivity = Phased(0.87, 1.42);
-    mMoistureTotal = 0.48;
-    mDryDensity = 1090;
-    mUnfrozenWaterCurve[-0.19]   =       49.0 / 100.0;
-    mUnfrozenWaterCurve[-0.2]   =       44.6 / 100.0;
-    mUnfrozenWaterCurve[-0.28]  =       12.75 / 100.0;
-    mUnfrozenWaterCurve[-0.7]   =       8.5 / 100.0;
-    mUnfrozenWaterCurve[-2]     =       5.4 / 100.0;
-    mUnfrozenWaterCurve[-5.6]   =       4.6 / 100.0;
-    mUnfrozenWaterCurve[-14]    =       3.6 / 100.0;*/
-
-    //21
-    /*mCapacity = Phased(2207500, 1351730);
-    mConductivity = Phased(0.92, 1.68);
-    mMoistureTotal = 0.732;
-    mDryDensity = 900;
-    mUnfrozenWaterCurve[-0.24]  =       80.0 / 100.0;
-    mUnfrozenWaterCurve[-0.25]  =       72.9 / 100.0;
-    mUnfrozenWaterCurve[-2.02]  =       11.0 / 100.0;
-    mUnfrozenWaterCurve[-2.8]   =       3.0 / 100.0;
-    mUnfrozenWaterCurve[-5.6]   =       2.5 / 100.0;
-    mUnfrozenWaterCurve[-14]    =       2.5 / 100.0;*/
-
-    //23
-    /*mCapacity = Phased(2344360, 1452430);
-    mConductivity = Phased(2.68, 1.27);
-    mMoistureTotal = 0.802;
-    mDryDensity = 830;
-    mUnfrozenWaterCurve[-0.1]   =       90.4 / 100.0;
-    mUnfrozenWaterCurve[-0.4]   =       35.0 / 100.0;
-    mUnfrozenWaterCurve[-0.9]   =       15.5 / 100.0;
-    mUnfrozenWaterCurve[-2.5]   =       4.2 / 100.0;
-    mUnfrozenWaterCurve[-5.6]   =       3.5 / 100.0;
-    mUnfrozenWaterCurve[-14]    =       3.3 / 100.0; */
-
-    //26
-    /*mCapacity = Phased(1387790, 983550);
-    mConductivity = Phased(1.12, 1.76);
-    mMoistureTotal = 0.245;
-    mDryDensity = 1540;
-    mUnfrozenWaterCurve[-0.25]  =       26.0 / 100.0;
-    mUnfrozenWaterCurve[-0.74]  =       10.0 / 100.0;
-    mUnfrozenWaterCurve[-2.0]     =       1.4 / 100.0;
-    mUnfrozenWaterCurve[-5.6]   =       1.3 / 100.0;
-    mUnfrozenWaterCurve[-14.0]    =       1.2 / 100.0; */
-
-    //29
-    /*
-    mCapacity = Phased(1612400, 1186690);
-    mConductivity = Phased(0.81, 1.11);
-    mMoistureTotal = 0.334;
-    mDryDensity = 1320;
-    mUnfrozenWaterCurve[-0.74]  =       34.0 / 100.0;
-    mUnfrozenWaterCurve[-0.78]  =       30.3 / 100.0;
-    mUnfrozenWaterCurve[-2.1]   =       20.7 / 100.0;
-    mUnfrozenWaterCurve[-2.9]   =       15.3 / 100.0;
-    mUnfrozenWaterCurve[-5.6]   =       9.1 / 100.0;
-    mUnfrozenWaterCurve[-14.0]    =       6.3 / 100.0; */
-
-    //30
-    /*
-    mCapacity = Phased(1772090, 1282650);
-    mConductivity = Phased(0.99, 0.77);
-    mMoistureTotal = 0.400;
-    mDryDensity = 1290;
-    mUnfrozenWaterCurve[-1.7]   =       40 / 100.0;
-    mUnfrozenWaterCurve[-2.1]   =       22.9 / 100.0;
-    mUnfrozenWaterCurve[-6.2]   =       12.5 / 100.0;
-    mUnfrozenWaterCurve[-20.5]  =       7.4 / 100.0;*/
-
-    //31
-    /*mCapacity = Phased(1603650, 1182020);
-    mConductivity = Phased(1.21, 1.02);
-    mMoistureTotal = 0.329;
-    mDryDensity = 1440;
-    mUnfrozenWaterCurve[-1.6]   =       32.9 / 100.0;
-    mUnfrozenWaterCurve[-2.1]   =       19.4 / 100.0;
-    mUnfrozenWaterCurve[-6.2]   =       10.7 / 100.0;
-    mUnfrozenWaterCurve[-20.5]  =       6.2 / 100.0;*/
-
-    //32
     mCapacity = Phased(1206870, 909320);
     mConductivity = Phased(1.31, 1.38);
     mMoistureTotal = 0.186;
     mDryDensity = 1550;
-    mUnfrozenWaterCurve[-0.5]   =       18.6 / 100.0;
-    mUnfrozenWaterCurve[-2.1]   =       8.4 / 100.0;
-    mUnfrozenWaterCurve[-6.2]   =       3.3 / 100.0;
-    mUnfrozenWaterCurve[-20.5]  =       1.8 / 100.0;
+    mUnfrozenWaterCurve[-0.5]  = 18.6 / 100.0;
+    mUnfrozenWaterCurve[-2.1]  = 8.4 / 100.0;
+    mUnfrozenWaterCurve[-6.2]  = 3.3 / 100.0;
+    mUnfrozenWaterCurve[-20.5] = 1.8 / 100.0;
 
     calcIEConductivity();
     calcEnthalpy();
@@ -378,7 +211,8 @@ void SoilBlock::setThawedPart(double value)
     assert(thawedPartIsOk());
 }
 
-void SoilBlock::setAllProperties(const SoilBlock &other, bool mustNormalizeThawedPart)
+void SoilBlock::setAllProperties(const SoilBlock &other,
+                                 bool mustNormalizeThawedPart)
 {
     mCapacity = other.mCapacity;
     mTransitionHeat = other.mTransitionHeat;
