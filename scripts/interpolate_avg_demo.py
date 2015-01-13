3db44abd3bd4c65ea9702aba6949853a96066bf4
Denis Pesotsky <denis@kde.ru>
2015-01-13 11:18:51 +0300

    interpolate_avg demo: more modular and nicer code

--- scripts/interpolate_avg_demo.py
+++ scripts/interpolate_avg_demo.py
@@ -3,11 +3,11 @@
 
 import numpy as np
 import datetime as dt
+from warnings import warn
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as mticker
 from interpolate_avg import spline_avg_coeffs, interp_avg
-
 
 def _polynom_str(x0, coeffs):
     """Понятное человеку текстовое представление многочлена N степени.
@@ -32,14 +32,38 @@ def _polynom_str(x0, coeffs):
     return result
 
 def _next_range(arr):
-    if isinstance(arr[0], dt.date):
+    """Возвращает массив, полученный прибавлением к `arr` значения `arr[-1]`."""
+    if not isinstance(arr[0], dt.date):
+        return arr + arr[-1]
+    else:
         result = arr
         delta = arr[-1] - arr[0]
         for i in range(0, len(arr)):
             result[i] += delta
         return result
-    else:
-        return arr + arr[-1]
+
+
+def _step_func(x, y):
+    """Возвращает пару массивов, задающих ступенчатую функцию для её построения.
+
+    Параметры:
+    x: N+1 значений аргумента, задающих N интервалов. Должен быть возрастающим!
+    y: N значений функции для каждого из выделенных интервалов.
+    """
+    sx = []
+    sy = []
+
+    for i in range(0, len(y)):
+        sx.append(x[i])
+        sy.append(y[i])
+
+        sx.append(x[i+1])
+        sy.append(y[i])
+
+    sx.append(x[-1])
+    sy.append(y[0])
+
+    return sx, sy
 
 
 def _roman(input):
@@ -58,6 +82,31 @@ def _roman(input):
     return result
 
 
+def _setup_monthly_axis(axis):
+    """Настройка засечек и подписей `axis` для выделения ежемесячных интервалов.
+
+    Результат: помесячное разбиение оси с указанием номера каждого месяца в виде
+    римских цифр, расположенных по центру каждого интервала. Эту функцию следует
+    использовать после проведения построений или задания масштаба, поскольку она
+    должна убрать серединные засечки, появляющиеся после центрирования подписей.
+    """
+    axis.set_major_locator(mdates.MonthLocator())
+    axis.set_minor_locator(mdates.MonthLocator(bymonthday=15))
+
+    def month_roman(x, pos):
+        return _roman(mdates.num2date(x).month)
+
+    axis.set_major_formatter(mticker.NullFormatter())
+    axis.set_minor_formatter(mticker.FuncFormatter(month_roman))
+
+    minor_ticks = axis.get_minor_ticks()
+    if not minor_ticks:
+        warn('no minor ticks: check that plotting/scaling is done before call')
+    else:
+        for tic in minor_ticks:
+            tic.tick1On = tic.tick2On = False
+
+
 def main():
     y = np.array([-20.7, -21.7, -19.5, -12.9, -5.2, 1.9,
                   7.4, 6.9, 3.4, -3.9, -12.5, -18.1])
@@ -67,24 +116,13 @@ def main():
 
     result = spline_avg_coeffs(x, y)
 
-    sx = []
-    sy = []
-
-    print('x_i\tx_i+1\ty_i\tf_i(x)')
-    for i in range(0, len(y)):
-        polynom = _polynom_str(x[i], result[i])
-        print('{}\t{}\t{}\t{}'.format(x[i], x[i+1], y[i], polynom))
-
-        sx.append(x[i])
-        sy.append(y[i])
-
-        sx.append(x[i+1])
-        sy.append(y[i])
-
-    sx.append(x[-1])
-    sy.append(y[0])
+    #print('x_i\tx_i+1\ty_i\tf_i(x)')
+    #for i in range(0, len(y)):
+    #    polynom = _polynom_str(x[i], result[i])
+    #    print('{}\t{}\t{}\t{}'.format(x[i], x[i+1], y[i], polynom))
 
     ax, ay = interp_avg(x, y, 1000)
+    sx, sy = _step_func(x, y)
 
     fig, axes = plt.subplots()
 
@@ -94,18 +132,7 @@ def main():
     axes.plot(_next_range(sx), sy, antialiased=False, color='gray', ls='--')
 
     if isinstance(x[0], dt.date):
-        xaxis = axes.xaxis
-        xaxis.set_major_locator(mdates.MonthLocator())
-        xaxis.set_minor_locator(mdates.MonthLocator(bymonthday=15))
-
-        def month_roman(x, pos):
-            return _roman(mdates.num2date(x).month)
-
-        xaxis.set_major_formatter(mticker.NullFormatter())
-        xaxis.set_minor_formatter(mticker.FuncFormatter(month_roman))
-
-        for tic in xaxis.get_minor_ticks():
-            tic.tick1On = tic.tick2On = False
+        _setup_monthly_axis(axes.xaxis)
 
     axes.grid()
 
